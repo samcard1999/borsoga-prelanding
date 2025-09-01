d19d5bd196e84c8ee32eb343a3209e7b682a2ccd
Samuel Cardenas <samcard1999@gmail.com>
2025-08-31 23:47:19 -0400

    logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,11 @@ export default {
       borderWidth: {
         DEFAULT: '0.5px', // <- ahora border y border-t/-b/-l/-r serán de 1.5px
       },
+      screens: {
+        'md-860px': '860px',
+        '2xl-custom': '2000px', // Define un nuevo breakpoint para un ancho mínimo de 2000px
+        '3xl-custom': '3200px', // Define un nuevo breakpoint para un ancho mínimo de 3200px
+      },
     },
   },
   plugins: [],
